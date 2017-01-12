9e2cef2af9c7fec039a988e1ce6869765831d27a
Ailian <wumh@cncn.com>
2017-01-12 18:02:08 +0800

    添加学习内容

--- first/pages/cal/cal.js
+++ first/pages/cal/cal.js
@@ -1,7 +1,22 @@
 Page({
     data:{
         name:"wmh",
-        age:12
+        age:12,
+        id:1,
+        condit:true,
+        flag:false,
+        operA:2,
+        operB:3,
+        operC:3,
+        size:6,
+        obj:{text:"数据路径运算:"},
+        fa:"对象",
+        ba:"组合",
+
+
+        array:[1,2,3,4,5],
+        ch:"jifen",
+        userInfoA:{firstname:"良",lastname:"生"},
     },
     clickbtn:function(){
         this.setData({
